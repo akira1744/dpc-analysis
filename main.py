d2cf7f39d5c830efb2ec3693453b9abd64589278
akira1744 <akr1744@gmail.com>
2024-03-08 16:54:40 +0900

    Mod DataSource

--- main.py
+++ main.py
@@ -49,24 +49,18 @@ st.altair_chart(charts)
 
 
 # フッター　###################################################################################
-link1 = "https://www.mhlw.go.jp/stf/shingi2/0000196043_00005.html"
+link1 = "https://www.mhlw.go.jp/stf/shingi2/0000196043_00006.html"
 link2 = "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000198757_00003.html"
-link3 = "https://www.e-stat.go.jp/stat-search/files?page=1&query=%E7%97%85%E9%99%A2%E6%95%B0%E3%80%80%E7%97%85%E5%BA%8A%E6%95%B0%E3%80%80%E4%BA%8C%E6%AC%A1%E5%8C%BB%E7%99%82%E5%9C%8F&sort=open_date%20desc&layout=dataset&stat_infid=000031982297&metadata=1&data=1"
 
 my_expander = st.expander("DataSource")
 with my_expander:
     st.markdown(
-        "[1.令和2年度DPC導入の影響評価に係る調査「退院患者調査」の結果報告について]({})".format(
+        "[1.令和3年度DPC導入の影響評価に係る調査「退院患者調査」の結果報告について]({})".format(
             link1
         )
     )
     st.markdown(
         "[2.診断群分類（DPC) 電子点数表（令和3年11月24日更新）]({}) ".format(link2)
-    )
-    st.markdown(
-        "[3.医療施設調査 / 令和元年医療施設（動態）調査 二次医療圏・市区町村編]({})".format(
-            link3
-        )
     )
 
 my_expander = st.expander("Q & A")
